30fde778f3c2dd458128411b294be374c32ae845
dettyboy <dettyboy@icloud.com>
2025-10-18 01:00:47 +0100

    refactor: trade page done and refactor

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -103,10 +103,54 @@ class Homepage extends StatelessWidget {
                       const QuickActionCard(),
                       SizedBox(height: 25.h),
                       Container(
+                        width: double.infinity,
                         height: 112.h,
                         decoration: BoxDecoration(
-                          color: AppColors.bgColor,
-                          borderRadius: BorderRadius.circular(16.r),
+                            color: AppColors.bgColor,
+                            borderRadius: BorderRadius.circular(16.r),
+                            gradient: const LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [
+                                Color(0xFFABE2F3),
+                                Color(0xFFBDE4E5),
+                                Color(0xFFEBE9D0),
+                              ],
+                              stops: [0.0, 0.5, 1.0],
+                            )),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 16.0, top: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Copy Trading',
+                                    style: GoogleFonts.encodeSans(
+                                      color: AppColors.primaryColor,
+                                      fontSize: 16.sp,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 4.h,
+                                  ),
+                                  Text(
+                                    'Discover our latest feature. Follow and watch\n the PRO traders closely and win like a PRO!\n We are rooting for you!',
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Image.asset('assets/images/crown.png')
+                          ],
                         ),
                       ),
                       SizedBox(height: 25.h),
--- lib/views/trade/trade.dart
+++ lib/views/trade/trade.dart
@@ -1,279 +1,235 @@
+import 'dart:math';
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:roqqu_task/utils/app_colors.dart';
+import 'package:roqqu_task/utils/app_text_styles.dart';
 
-class Trade extends StatelessWidget {
+class Trade extends StatefulWidget {
   const Trade({super.key});
 
   @override
+  State<Trade> createState() => _TradeState();
+}
+
+class _TradeState extends State<Trade> {
+  final List<String> tradeOptions = [
+    'Buy',
+    'Sell',
+    'Swap',
+    'Send',
+    'Receive',
+    'Invest'
+  ];
+
+  final List<String> earnOptions = [
+    'Roqq n roll',
+    'Savings',
+    'Savings',
+    'Mission',
+    'Copy trading',
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.centerLeft,
-        end: Alignment.centerRight,
-        colors: [
-          Color(0xFFC0CFFE),
-          Color(0xFFF3DFF4),
-          Color(0xFFF9D8E5),
-        ],
-        stops: [0.0, 0.7, 1.0],
-      )),
-      child: SafeArea(
-          child: Column(
+      body: Stack(
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 25.h),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color(0xFFC0CFFE),
+                Color(0xFFF3DFF4),
+                Color(0xFFF9D8E5),
+              ],
+              stops: [0.0, 0.7, 1.0],
+            )),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 20, sigmaY: 20),
+              child: Container(color: Colors.transparent),
+            ),
+          ),
+          SafeArea(
+            child: Column(
               children: [
-                const Text('Crypto'),
-                Row(
-                  children: [
-                    Image.asset('assets/icons/search.png'),
-                    SizedBox(width: 15.w),
-                    Image.asset('assets/icons/headphones.png'),
-                    SizedBox(width: 15.w),
-                    Image.asset('assets/icons/notification.png'),
-                    SizedBox(width: 15.w),
-                  ],
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.w, vertical: 45.h),
+                  // child: Row(
+                  //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //   children: [
+                  //     const Text('Crypto'),
+                  //     Row(
+                  //       children: [
+                  //         Image.asset('assets/icons/search.png'),
+                  //         SizedBox(width: 15.w),
+                  //         Image.asset('assets/icons/headphones.png'),
+                  //         SizedBox(width: 15.w),
+                  //         Image.asset('assets/icons/notification.png'),
+                  //         SizedBox(width: 15.w),
+                  //       ],
+                  //     )
+                  //   ],
+                  // ),
+                ),
+                Expanded(
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30.r),
+                          topRight: Radius.circular(30.r),
+                        ),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                          child: Container(
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: AppColors.primaryColor.withOpacity(0.8),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30.r),
+                                topRight: Radius.circular(30.r),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 16.w,
+                        left: 16.w,
+                        top: -55.h,
+                        bottom: 0,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16.w, vertical: 24.h),
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(24.r),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 10,
+                                offset: const Offset(0, 4),
+                              ),
+                            ],
+                          ),
+                          child: SingleChildScrollView(
+                            physics: const BouncingScrollPhysics(),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Trade',
+                                  style: AppTextStyles.heading,
+                                ),
+                                SizedBox(
+                                  height: 16.h,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(16.r),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.bgColor,
+                                    borderRadius: BorderRadius.circular(16.r),
+                                  ),
+                                  child: Column(
+                                    children: List.generate(tradeOptions.length,
+                                        (index) {
+                                      final option = tradeOptions[index];
+                                      return Column(
+                                        children: [
+                                          ListTile(
+                                            contentPadding: EdgeInsets.zero,
+                                            leading: Image.asset(
+                                                'assets/icons/trade.png'),
+                                            title: Text(
+                                              option,
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 14.sp,
+                                              ),
+                                            ),
+                                            trailing: const Icon(
+                                                Icons.navigate_next,
+                                                color: Colors.white),
+                                          ),
+                                          if (index != tradeOptions.length - 1)
+                                            Divider(
+                                              thickness: 1.h,
+                                              color: AppColors.borderColor,
+                                              height: 8.h,
+                                            ),
+                                        ],
+                                      );
+                                    }),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 16.h,
+                                ),
+                                Text(
+                                  'Earn',
+                                  style: AppTextStyles.heading,
+                                ),
+                                SizedBox(
+                                  height: 16.h,
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(16.r),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.bgColor,
+                                    borderRadius: BorderRadius.circular(16.r),
+                                  ),
+                                  child: Column(
+                                    children: List.generate(earnOptions.length,
+                                        (index) {
+                                      final option = earnOptions[index];
+                                      return Column(
+                                        children: [
+                                          ListTile(
+                                            contentPadding: EdgeInsets.zero,
+                                            leading: Image.asset(
+                                                'assets/icons/trade.png'),
+                                            title: Text(
+                                              option,
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 14.sp,
+                                              ),
+                                            ),
+                                            trailing: const Icon(
+                                                Icons.navigate_next,
+                                                color: Colors.white),
+                                          ),
+                                          if (index != earnOptions.length - 1)
+                                            Divider(
+                                              thickness: 1.h,
+                                              color: AppColors.borderColor,
+                                              height: 8.h,
+                                            ),
+                                        ],
+                                      );
+                                    }),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
-          ),
+          )
         ],
-      )),
-    ));
+      ),
+    );
   }
 }
-
-
-
-
-
-// import 'dart:ui';
-// import 'package:flutter/material.dart';
-// import 'package:iconsax_flutter/iconsax_flutter.dart'; // optional
-
-// class Trade extends StatelessWidget {
-//   const Trade({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       extendBody: true, // allows content behind bottom nav
-//       backgroundColor: const Color(0xFF0E0E10),
-//       bottomNavigationBar: _buildBottomNav(),
-//       body: Stack(
-//         children: [
-//           // Gradient background
-//           Container(
-//             decoration: const BoxDecoration(
-//               gradient: LinearGradient(
-//                 begin: Alignment.topCenter,
-//                 end: Alignment.bottomCenter,
-//                 colors: [
-//                   Color(0xFFDCCBFF),
-//                   Color(0xFF121212),
-//                 ],
-//               ),
-//             ),
-//           ),
-
-//           // Main content scroll
-//           SingleChildScrollView(
-//             padding: const EdgeInsets.only(top: 80),
-//             child: Column(
-//               children: [
-//                 _buildTopSelector(),
-//                 const SizedBox(height: 20),
-//               ],
-//             ),
-//           ),
-
-//           // Blurry bottom sheet overlay
-//           Align(
-//             alignment: Alignment.bottomCenter,
-//             child: ClipRRect(
-//               borderRadius: const BorderRadius.only(
-//                 topLeft: Radius.circular(24),
-//                 topRight: Radius.circular(24),
-//               ),
-//               child: BackdropFilter(
-//                 filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-//                 child: Container(
-//                   width: double.infinity,
-//                   padding: const EdgeInsets.all(20),
-//                   decoration: BoxDecoration(
-//                     color: Colors.white.withOpacity(0.05),
-//                     border: Border.all(
-//                       color: Colors.white.withOpacity(0.1),
-//                     ),
-//                     borderRadius: const BorderRadius.only(
-//                       topLeft: Radius.circular(24),
-//                       topRight: Radius.circular(24),
-//                     ),
-//                   ),
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     mainAxisSize: MainAxisSize.min,
-//                     children: [
-//                       _buildSection("Trade", [
-//                         "Deposit",
-//                         "Buy",
-//                         "Withdraw",
-//                         "Sell",
-//                         "Swap",
-//                         "Send",
-//                         "Receive",
-//                         "Transfer",
-//                       ]),
-//                       const SizedBox(height: 20),
-//                       _buildSection("Earn", ["Airdrops", "Savings"]),
-//                       const SizedBox(height: 20),
-//                       const Text(
-//                         "Games",
-//                         style: TextStyle(
-//                           color: Colors.grey,
-//                           fontSize: 16,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                       const SizedBox(height: 20),
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildSection(String title, List<String> items) {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         Text(
-//           title,
-//           style: const TextStyle(
-//             color: Colors.grey,
-//             fontSize: 16,
-//             fontWeight: FontWeight.bold,
-//           ),
-//         ),
-//         const SizedBox(height: 12),
-//         Container(
-//           decoration: BoxDecoration(
-//             color: Colors.white.withOpacity(0.05),
-//             borderRadius: BorderRadius.circular(16),
-//             border: Border.all(color: Colors.white.withOpacity(0.1)),
-//           ),
-//           child: Column(
-//             children: items
-//                 .map(
-//                   (e) => ListTile(
-//                     dense: true,
-//                     leading: const Icon(Iconsax.arrow_circle_down,
-//                         color: Colors.blueAccent),
-//                     title: Text(
-//                       e,
-//                       style: const TextStyle(color: Colors.white, fontSize: 14),
-//                     ),
-//                     trailing:
-//                         const Icon(Icons.chevron_right, color: Colors.grey),
-//                   ),
-//                 )
-//                 .toList(),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-
-//   Widget _buildTopSelector() {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 16),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Container(
-//             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//             decoration: BoxDecoration(
-//               color: Colors.white.withOpacity(0.15),
-//               borderRadius: BorderRadius.circular(30),
-//             ),
-//             child: const Row(
-//               children: [
-//                 Text(
-//                   "Crypto",
-//                   style: TextStyle(
-//                       color: Colors.white, fontWeight: FontWeight.w500),
-//                 ),
-//                 Icon(Icons.keyboard_arrow_down, color: Colors.white),
-//               ],
-//             ),
-//           ),
-//           const Row(
-//             children: [
-//               Icon(Icons.search, color: Colors.white),
-//               SizedBox(width: 16),
-//               Icon(Icons.headphones, color: Colors.white),
-//               SizedBox(width: 16),
-//               Icon(Icons.notifications_none, color: Colors.white),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _buildBottomNav() {
-//     return BottomAppBar(
-//       color: Colors.black.withOpacity(0.8),
-//       shape: const CircularNotchedRectangle(),
-//       notchMargin: 8,
-//       child: SizedBox(
-//         height: 70,
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceAround,
-//           children: const [
-//             _NavItem(icon: Iconsax.home, label: "Home", active: true),
-//             _NavItem(icon: Iconsax.wallet_2, label: "Wallet"),
-//             SizedBox(width: 40),
-//             _NavItem(icon: Iconsax.clock, label: "History"),
-//             _NavItem(icon: Iconsax.user, label: "Profile"),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class _NavItem extends StatelessWidget {
-//   final IconData icon;
-//   final String label;
-//   final bool active;
-
-//   const _NavItem(
-//       {required this.icon, required this.label, this.active = false});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         Icon(icon, color: active ? Colors.white : Colors.grey),
-//         Text(
-//           label,
-//           style: TextStyle(
-//             color: active ? Colors.white : Colors.grey,
-//             fontSize: 12,
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
